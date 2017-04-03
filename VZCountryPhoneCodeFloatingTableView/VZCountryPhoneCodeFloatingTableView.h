5a0adf290dd36513728021b9ea293c4013727da3
Ehab Ashraf <ehab.ashraf@drbridge.com>
2017-04-03 23:56:42 +0200

    Initial Commit

---
+++ VZCountryPhoneCodeFloatingTableView/VZCountryPhoneCodeFloatingTableView.h
@@ -0,0 +1,19 @@
+//
+//  VZCountryPhoneCodeFloatingTableView.h
+//  VZCountryPhoneCodeFloatingTableView
+//
+//  Created by Ehab Ashraf on 4/3/17.
+//  Copyright © 2017 Vezeeta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VZCountryPhoneCodeFloatingTableView.
+FOUNDATION_EXPORT double VZCountryPhoneCodeFloatingTableViewVersionNumber;
+
+//! Project version string for VZCountryPhoneCodeFloatingTableView.
+FOUNDATION_EXPORT const unsigned char VZCountryPhoneCodeFloatingTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VZCountryPhoneCodeFloatingTableView/PublicHeader.h>
+
+
